Fixed minibatching to work with the basic decoder. Added masking to the batched decoding as well.

encdec.py
@@ -33,13 +33,13 @@ class RNNEncoder(nn.Module):
    # src is a batch of sentences
     def forward(self, src):
         embedded = self.embedding(src)  # 3D Tensor of size [batch_size x num_hist x emb_size]
-        print(embedded.size())
+        #print("Embedding size: ", embedded.size())
         batch_size = embedded.shape[0]
-        print(batch_size)
+        #print(batch_size)
         #feat = embedded.view(embedded.size(0), -1) # 2D Tensor of size [batch_size x (num_hist*emb_size)]
         #print("Emb shape: {}".format(embedded.shape))
         output, self.hidden = self.rnn(embedded, self.hidden)
-        print(output.size(), self.hidden.size())
+        #print(output.size(), self.hidden.size())
         # embedded = self.embedding(src)
         # output, self.hidden = self.rnn(embedded.view(len(src), 1, -1), self.hidden)
         return output, self.hidden
@@ -58,52 +58,56 @@ class RNNDecoder(nn.Module):
         self.hidden_size = hidden_size
         self.embedding   = nn.Embedding(vocab_size, embed_size)
         self.num_layers  = num_layers
-        self.rnn = rnn_factory(rnn_type, input_size=embed_size, hidden_size=hidden_size, num_layers=num_layers, bidirectional=bidirectional)  #nn.rnn internally makes input_size=hidden_size for >1 layer
+        self.rnn = rnn_factory(rnn_type, input_size=embed_size, hidden_size=hidden_size, batch_first=True,
+                               num_layers=num_layers, bidirectional=bidirectional)  #input_size will need to change if num_layers>1 !
         self.out = nn.Linear(hidden_size, vocab_size)
-        self.softmax = nn.LogSoftmax(dim=1)  #dim corresponding to vocab
-        #self.hidden = self.init_hidden()
+        self.softmax = nn.LogSoftmax(dim=2)  #dim corresponding to vocab
+        self.hidden = None
 
     # Generates entire sequence, up to tgt_len, conditioned on the initial hidden state
+    # Note that this decoder does not use the encoder_outputs at all
     def forward(self, init_hidden, encoder_outputs, tgt_len, generate=False):
-        self.hidden = init_hidden  #init hidden with last encoder hidden
-        decoder_input = Variable(torch.LongTensor([[SOS]]))
+        self.hidden = init_hidden
+        #print("Init hidden state of decoder: ", init_hidden.size())
+        # The hidden state in RNNs in Pytorch is always (seq_length, batch_size, emb_size) - even if you use batch_first
+        decoder_input = Variable(torch.LongTensor(init_hidden.shape[1] * [[SOS]]))
+        #print("Init input to decoder shape: ", decoder_input.size())
         decoder_input = decoder_input.cuda() if use_cuda else decoder_input
         outputs = []
         words = []
         for _ in range(tgt_len):
-            decoder_output = self.__forward_one_word(decoder_input)
-            _, top_idx = decoder_output.data.topk(1)
+            decoder_outputs = self.__forward_one_word(decoder_input)
+            _, top_idx = decoder_outputs.data.topk(1)
             # Recover the value of the top index and wrap in a new variable to break backprop chain
-            word_idx = top_idx[0][0]
-            decoder_input = Variable(torch.LongTensor([[word_idx]]))
+            word_idx = top_idx.cpu().numpy()
+            # Results in a new decoder input of dims (batch_size, 1)
+            decoder_input = Variable(torch.LongTensor(torch.from_numpy(word_idx))).squeeze(2)
             decoder_input = decoder_input.cuda() if use_cuda else decoder_input
             if generate:
                 words.append(word_idx)
-            outputs.append(decoder_output)
-            if word_idx == EOS:
-                break
+            outputs.append(decoder_outputs.squeeze(1))
+            #print("New input to decoder shape: ", decoder_input.size())
+            # if word_idx == EOS:
+            #     break
         return outputs, words
 
     # Passes a single word through the decoder network
     def __forward_one_word(self, tgt):
         # Map to embeddings - dims are (seq length, batch size, emb size)
-        output = self.embedding(tgt).view(1, 1, -1)
+        batch_size = tgt.shape[0]
+        output = self.embedding(tgt).view(batch_size, 1, -1)
+        #print("Dimensions after embedding layer: ", output.size())
+        # Non-linear activation over embeddings
+        output = F.tanh(output)
+        #print("Dimensions after non-linearity", output.size())
         # Pass representation through RNN
         output, self.hidden = self.rnn(output, self.hidden)
+        #print("Dimensions after RNN", output.size(), self.hidden.size())
         # Softmax over the final output state
-        output = self.softmax(self.out(output[0]))
+        output = self.softmax(self.out(output))
+        #print("Dimensions after softmax", output.size())
 
         return output
-
-    #Initialize hidden state to a pair of variables for context/hidden
-    def init_hidden(self):
-        return None
-        result = (Variable(torch.zeros(1, 1, self.hidden_size)),
-                  Variable(torch.zeros(1, 1, self.hidden_size)))
-        if use_cuda:
-            return result[0].cuda(), result[1].cuda()
-        else:
-            return result
 
     def save(self, fname):
         """Save the model to a pickle file."""
@@ -221,13 +225,16 @@ class EncDec(nn.Module):
 
     def forward(self, src, tgt, batch_size=1):
         # TODO tgt.shape[0] may be wrong in this call below
-        return self._forward(src, tgt.shape[0], batch_size)
+        return self._forward(src, tgt.shape[1], batch_size)
 
     def generate(self, src, max_length, batch_size=1):
-        return self._forward(src, max_length, batch_size, generate=True)
+        return self._forward(src, max_length, generate=True)
 
     # src,tgt currently single sentences
-    def _forward(self, src, tgt_len, batch_size, generate=False):
+    def _forward(self, src, tgt_len, generate=False):
+        batch_size = src.shape[0]
+        #print("Source shape: ", src.shape)
+        #print("Batch size: ", batch_size)
         self.encoder.hidden = self.encoder.init_hidden(batch_size)
         encoder_outputs, encoder_hidden = self.encoder(src)
         decoder_outputs, words = \

training.py
@@ -7,13 +7,15 @@ from torch import optim
 from torch.autograd import Variable
 
 #local imports
-from utils import use_cuda, pair2var, OUTPUT_PATH
-from preprocessing import SOS, EOS
+from utils import time_elapsed, save_plot, use_cuda, pair2var, perplexity, MODEL_PATH, OUTPUT_PATH
+from preprocessing import SOS, EOS, EOS_TOKEN
 from batching import make_batches
 
 import logging
 
 logger = logging.getLogger(__name__)
+
+random.seed(69)
 
 def optimizer_factory(optim_type, model, **kwargs):
     assert optim_type in ['SGD', 'Adam'], 'Optimizer type not one of currently supported options'
@@ -25,7 +27,8 @@ class MTTrainer:
     def __init__(self, model, train_monitor, optim_type='SGD', learning_rate=0.01):
         self.model = model
         self.optimizer = optimizer_factory(optim_type, model, lr=learning_rate)
-        self.loss_fn = nn.NLLLoss()
+        # Reduce flag makes this return a loss per patch, necessary for masking
+        self.loss_fn = nn.NLLLoss(reduce=False)
         self.use_nllloss = True
         self.monitor = train_monitor
 
@@ -33,12 +36,30 @@ class MTTrainer:
 
         self.optimizer.zero_grad()
         loss = 0.0
-        tgt_length = tgt.size()[0]
-
+        #print("Source shape", src.shape, "Target shape", tgt.shape)
+        # Dimensions are (batch_size, sequence_length)
+        tgt_length = tgt.shape[1]
+        # print("Target length: ", tgt_length)
         decoder_scores, words = self.model(src, tgt)
+        # print(len(decoder_scores))
+        # print(decoder_scores[0].shape)
+        # print(tgt)
 
-        for gen, ref in zip(decoder_scores, tgt):
-            loss += self.loss_fn(gen, ref)
+        # Compute masks by looking at # of EOS symbols in each sentence, then create masks with (# EOS - 1) 0's at end
+        padding = tgt.eq(EOS).sum(1).sub(1)
+        masks = Variable(torch.FloatTensor([[[1] * (tgt_length - i) + [0] * i] for i in padding.data])).squeeze(1)
+        if use_cuda:
+            masks = masks.cuda()
+        #print("Masks: ", masks.shape)
+
+        for gen, ref, mask in zip(decoder_scores, tgt.transpose(0, 1), masks.transpose(0, 1)):
+            # print("Mask", mask)
+            # print(gen.size(), ref.size())
+            losses = self.loss_fn(gen, ref)
+            losses = losses * mask
+            # print(losses.mean())
+            # loss += losses.
+            loss += losses.mean()
 
         # todo: lecture 2/20 re loss fns. pre-train with teacher forcing, finalize using own predictions
 
@@ -49,13 +70,13 @@ class MTTrainer:
         return loss.data[0] / tgt_length
 
     def train(self, train_sents, dev_sents, tst_sents, src_vocab, tgt_vocab,
-              num_epochs, max_gen_length=100, checkpoint=20000, debug=False):
+              num_epochs, max_gen_length=100, debug=False):
 
         batch_size = 64
         batches = make_batches(train_sents, batch_size)
-        num_batches = len(train_sents)
+        num_batches = len(batches)
 
-        self.monitor.iters_per_epoch = num_batches
+        self.monitor.set_iters(num_batches)
         logger.info("Starting training:")
         self.monitor.start_training()
 
@@ -64,13 +85,13 @@ class MTTrainer:
         for ep in range(num_epochs):
             logger.info("Epoch %d:" % ep)
             if not debug:
-                random.shuffle(train_sents_vars) #note: should shuffle within batch when batching
+                random.shuffle(batches) #note: should shuffle within batch when batching
 
             for iteration in range(num_batches):
                 src_sent, tgt_sent = pair2var(batches[iteration])
 
-                batch_length = src_sent.size()[1]  # size of longest src sent in batch
-                loss = self.train_step(src_sent, tgt_sent, max_length=batch_length)
+                max_batch_length = src_sent.size()[1]  # size of longest src sent in batch
+                loss = self.train_step(src_sent, tgt_sent, max_length=max_batch_length)
 
                 self.monitor.finish_iter('train', loss)
 
@@ -79,6 +100,7 @@ class MTTrainer:
                 total_iters += 1
 
                 if total_iters % self.monitor.checkpoint == 0:
+                    logger.info("Calculating dev loss")
                     ep_fraction = (iteration + 1) / num_batches
                     dev_output_file = "dev_output_e{0}.{1}.txt".format(ep, ep_fraction)
                     avg_loss, total_loss = self.generate(dev_sents, src_vocab, tgt_vocab, max_gen_length, dev_output_file)
@@ -86,13 +108,14 @@ class MTTrainer:
 
             # end of epoch
             # generate output
+            logger.info("Calculating dev loss")
             dev_output_file = "dev_output_e{0}.txt".format(ep)
             avg_loss, total_loss = self.generate(dev_sents, src_vocab, tgt_vocab, max_gen_length, dev_output_file)
             self.monitor.finish_epoch(ep, 'dev', avg_loss, total_loss)
 
-            tst_output_file = "tst_output_e{0}.txt".format(ep)
-            avg_loss, total_loss = self.generate(tst_sents, src_vocab, tgt_vocab, max_gen_length, tst_output_file)
-            self.monitor.finish_epoch(ep, 'test', avg_loss, total_loss)
+            # tst_output_file = "tst_output_e{0}.txt".format(ep)
+            # avg_loss, total_loss = self.generate(tst_sents, src_vocab, tgt_vocab, max_gen_length, tst_output_file)
+            # self.monitor.finish_epoch(ep, 'test', avg_loss, total_loss)
 
         # todo: evaluate bleu
 
@@ -104,20 +127,29 @@ class MTTrainer:
 
         total_loss = 0.0
         output = []
-
+        num_processed = 0
         for sent in sents:
             src_ref = sent[0]
             tgt_ref = sent[1]
             sent_var = pair2var(sent)
             src_words = [src_vocab.idx2word[i] for i in src_ref]
             tgt_words = [tgt_vocab.idx2word[i] for i in tgt_ref]
-            scores, predicted = self.model.generate(sent_var[0], max_gen_length)
-            predicted_words = [tgt_vocab.idx2word[i] for i in predicted]
+            scores, predicted = self.model.generate(sent_var[0].view(1, len(src_words)), max_gen_length)
+            # todo: Either we should batch generation (possible?) or when we make the generate method,
+            # todo: don't have it spit out 3d arrays
+            predicted_words = [tgt_vocab.idx2word[i[0][0][0]] for i in predicted]
+            if EOS_TOKEN in predicted_words:
+                eos_index = predicted_words.index(EOS_TOKEN) + 1
+                predicted_words = predicted_words[:eos_index]
             # logger.info("Predicted:", predicted_words, "  Truth: ", tgt_words)
             output.append(" ".join(predicted_words))
             for gen, ref in zip(scores, sent_var[1]):
-                loss = self.loss_fn(gen, ref)
+                loss = self.loss_fn(gen, ref).mean()
                 total_loss += loss.data[0] / len(tgt_ref)
+            num_processed += 1
+            if num_processed % 100 == 0:
+                print("Processed {} sentences.".format(num_processed))
+
 
         with open(OUTPUT_PATH + '/' + output_file, 'w', encoding='utf-8') as f:
             f.write("\n".join(output))