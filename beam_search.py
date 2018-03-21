13cd8e05e130727ea12641ffdcac423b12ec9b6e
Alex Coda <coda.a@husky.neu.edu>
2018-03-21 17:39:51 -0400

    Beam search added, but not fully debuged

--- beam_search.py
+++ beam_search.py
@@ -1,68 +1,120 @@
 import torch
-from sortedcontainers import SortedList
-from preprocessing import EOS
+from torch.autograd import Variable
+from preprocessing import SOS, EOS
+from utils import use_cuda
 
 
 class Beam():
 
     def __init__(self, size):
         self.size = size
-        self.beams = {}
-        self.branches = {}
+        self.paths = {}
 
     def __iter__(self):
         """Iterates over the beams."""
-        return iter(self.beams)
+        return iter(self.paths)
+
+    def __len__(self):
+        """Return the size of the current beam."""
+        return len(self.paths)
 
     def __getitem__(self, path):
-        return self.beams[path]
+        """Returns the parameters needed to do a forward step in the decoder.
+
+        Returns: dict containing (decoder_outputs, decoder_context, score)
+        """
+        return self.paths[path]
+
+    def __setitem__(self, path, values):
+        """Add something to the paths."""
+        self.paths[path] = values
+
+    def get_score(self, path):
+        return self[path]['score']
 
     def get_decoder_params(self, path):
         """Returns the parameters needed to do a forward step in the decoder.
 
-        Returns: (decoder_outputs, decoder_context, attn_weights)
+        Returns: (decoder_inputs, decoder_context)
         """
-        params = self.beams[path]
-        return params['outputs'], params['context'], params['attn']
+        # Wrap the last word in the history in a Variable for the input
+        # TODO: Would need to change the size of this input for mini-batching
+        decoder_input = Variable(torch.LongTensor([[path[-1]]]))
+        decoder_input = decoder_input.cuda() if use_cuda else decoder_input
+        return decoder_input, self[path]['context']
 
-    def add_path(self, inputs, context, attn):
-        """Add a path to the beam search.
+    def add_initial_path(self, context):
+        """Add the first path to the beam search.
 
-        This should only be used to initialize the first beam (i.e. inputs = [SOS])
-
-        inputs: the decoder inputs, a list of vocabulary indices.
         context: the decoder context.
-        attn: the decoder attention weights.
         """
-        assert len(beams) < self.size, 'Tried to add a new beaazm path when already at max.'
-        path = tuple(inputs.squeeze().data.cpu().numpy())
-        self.beams[path] = {'inputs': inputs, 'outputs': None, 'context': context,
-                            'attn': attn, 'score': 0}
+        assert len(self) == 0, 'Tried to add a new beam path when already at max.'
+        path = (SOS, )
+        self[path] = {'outputs': [], 'context': context, 'score': 0}
 
-    def add_branches(self, history, outputs, context, attn):
-        """Add potential branching paths to be checked/pruned later."""
+    def delete_path(self, path):
+        """Delete a path from the dict of possible paths."""
+        return self.paths.pop(path)
+
+    def add_paths(self, path, outputs, context):
+        """Add potential branching paths to be checked/pruned later.
+
+        Adds the top self.size paths to the list of possible paths.
+        Deletes the original path passed in, as now we have the longer versions of it.
+        """
         top_nll, top_idx = decoder_outputs.data.topk(self.size)
-        self.branches[history] = {'outputs': outputs, 'context': context, 'attn': attn,
-                                  'candidate_idxs': top_idx, 'candidate_scores': top_nll}
+        top_idx = self.__to_array(top_idx)
+        top_nll = self.__to_array(top_nll)
+        # Get the historic score and outputs
+        hist_outputs = self[path]['outputs']
+        hist_score = self[path]['score']
+        new_outputs = hist_outputs
+        new_outputs.append(outputs)
 
-    def select_best_beams(self):
-        """Select the best beams to continue the beams search."""
-        best_scores = SortedList()
+        for idx, nll in zip(top_idx, top_nll):
+            new_path = path + (idx,)
+            new_score = hist_score + nll
+            self[new_path] = {'outputs': new_outputs, 'context': context,
+                'score': new_score}
+
+        # Finally we delete the historic path from the list of candidates
+        self.delete_path(path)
+
+    def select_best_paths(self):
+        """Select the best beam paths to continue the beam search."""
+        top_paths = self._get_topn_paths(self.size)
+        
+        # Go through and delete all other paths
         for path in self:
-            # Get the score of the history
-            # Get the scores for each candidate expansion
-            candidate_scores = None # TODO
-            for i, scores in best_scores:
-                pass
-                # Figure out
-            # Accumulate all (should be 5x5=25 candidates)
-        # Select the best ones
+            if path not in top_paths:
+                self.delete_path(path)
 
+        assert len(self) <= self.size, 'More than {} beams accumulated'.format(self.size)
 
-    def check_if_ended(self):
+    def get_best_path_results(self):
+        """Return the decoder outputs and word idxs from the best path."""
+        best_path = self._get_topn_paths(1)[0]
+        outputs = self[path]['outputs']
+        # Not sure why we do this, tbd...
+        outputs = [*map(lambda o: o.squeeze(1), outputs)]
+        return outputs, list(best_path)
+
+    def _get_topn_paths(self, n):
+        """Return a list of the top n paths."""
+        # Get the length normalized path scores
+        path_scores = {p: self[p]['score'] / (len(p) - 1) for p in self}
+        # Select the top paths from this
+        top_paths = sorted(path_scores, key=path_scores.get, reverse=True)[:n]
+        return top_paths
+
+    def is_ended(self):
         """Check if all beams have terminated."""
         for path in self:
             if path[-1] != EOS:
                 return False
         return True
+
+    def __to_array(self, arr):
+        """Convert a torch.cuda.LongTensor to a 1D numpy array."""
+        return arr.squeeze().cpu().numpy()
 
