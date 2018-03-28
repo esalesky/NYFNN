e82dab7b88b5e18061ad2f0a1978d613b4e6f282
Alex Coda <coda.a@husky.neu.edu>
2018-03-28 18:38:13 -0400

    Fixed beam search not correctly tracking hidden states.

--- beam_search.py
+++ beam_search.py
@@ -1,7 +1,11 @@
 import torch
 from torch.autograd import Variable
+from torch.nn import LogSoftmax
 from preprocessing import SOS, EOS
 from utils import use_cuda
+import pdb
+
+LS = LogSoftmax(dim=2)
 
 
 class Beam():
@@ -32,9 +36,6 @@ class Beam():
         """Add something to the paths."""
         self.paths[path] = values
 
-    def get_normalized_score(self, path):
-        return self[path]['score'] / (len(path) - 1)
-
     def get_decoder_params(self, path):
         """Returns the parameters needed to do a forward step in the decoder.
 
@@ -43,16 +44,17 @@ class Beam():
         # Wrap the last word in the history in a Variable for the input
         decoder_input = Variable(torch.LongTensor([[int(path[-1])]]))
         decoder_input = decoder_input.cuda() if use_cuda else decoder_input
-        return decoder_input, self[path]['context']
+        return decoder_input, self[path]['context'], self[path]['hidden']
 
-    def add_initial_path(self, context):
+    def add_initial_path(self, context, hidden):
         """Add the first path to the beam search.
 
         context: the decoder context.
         """
         assert len(self) == 0, 'Tried to add a new beam path when already at max.'
         path = (SOS, )
-        self[path] = {'outputs': [], 'attn_weights': [], 'context': context, 'score': 0}
+        self[path] = {'outputs': [], 'attn_weights': [], 'context': context, 'score': 0,
+            'hidden': hidden}
 
     def delete_path(self, path):
         """Delete a path from the dict of possible paths."""
@@ -62,15 +64,15 @@ class Beam():
         """Delete a candidate from the dict of possible candidates."""
         return self.candidates.pop(candidate)
 
-    def add_paths(self, path, outputs, attn, context):
+    def add_paths(self, path, outputs, attn, context, hidden):
         """Add potential branching paths to be checked/pruned later.
 
         Adds the top self.size paths to the list of possible paths.
         Deletes the original path passed in, as now we have the longer versions of it.
         """
+        # Get the top next chars and their probabilities
         top_nll, top_idx = outputs.data.topk(self.size)
-        top_idx = self.__to_array(top_idx)
-        top_nll = self.__to_array(top_nll)
+        top_nll, top_idx = self._process_topk(top_nll, top_idx)
         # Get the historic score and outputs
         hist_outputs = self[path]['outputs']
         new_outputs = hist_outputs
@@ -85,14 +87,13 @@ class Beam():
 
         for idx, nll in zip(top_idx, top_nll):
             new_path = path + (idx,)
+            new_score = hist_score + nll
+            params = {'outputs': new_outputs, 'attn_weights': new_attn,
+                'context': context, 'score': new_score, 'hidden': hidden}
             if idx == EOS:  # Add the sentence to the list of candidates
-                new_score = hist_score + nll * (self.source_len / len(new_path))
-                self.candidates[new_path] = {'outputs': new_outputs,
-                    'attn_weights': new_attn, 'context': context, 'score': new_score}
+                self.candidates[new_path] = params
             else:  # Keep it in the beam search
-                new_score = hist_score + nll
-                self[new_path] = {'outputs': new_outputs, 'attn_weights': new_attn,
-                      'context': context, 'score': new_score}
+                self[new_path] = params
 
         # Finally we delete the historic path from the list of candidates
         self.delete_path(path)
@@ -132,7 +133,7 @@ class Beam():
 
     def _get_topn_paths(self, n):
         """Return a list of the top n paths."""
-        return self._get_topn(self, n)
+        return self._get_topn(self, n, enforce_length=False)
 
     def _get_topn_candidates(self, n):
         """Return a list of the top n candidates."""
@@ -145,16 +146,27 @@ class Beam():
         if enforce_length:
             # This hyper param is based on the ratio of c_len / e_len
             # Currently it is just 1, but would be 0.77 for the sent len ratio
-            hyper_param = 0.77
-            length_penalty = lambda p: 1 + abs(hyper_param * self.source_len - len(p)) / len(p)
+            hyper_param = 1
+            length_penalty = lambda p: 1 + abs(hyper_param * self.source_len - len(p)) / self.source_len
             scores = {p: score * length_penalty(p) for p, score in scores.items()}
         # Select the top paths from this
         top_paths = sorted(scores, key=scores.get, reverse=True)[:n]
         return top_paths
 
-    def is_ended(self):
+    def _process_topk(self, nll, idx):
+        """Processes the topk next characters from the decoder."""
+        # First take the log-softmax of nll
+        nll = LS(Variable(nll))
+        # Then convert to numpy arrays
+        idx = self.__to_array(idx)
+        nll = self.__to_array(nll)
+        return nll, idx
+
+
+    def is_ended(self, curr_len):
         """Check if all beams have terminated."""
-        if self.ended_paths >= self.size and len(self.candidates) >= 1:
+        #if self.ended_paths >= self.size and len(self.candidates) >= 1:
+        if curr_len > self.source_len:
             # All the paths have EOS as their top choice
             return True
         else:
@@ -164,5 +176,9 @@ class Beam():
 
     def __to_array(self, arr):
         """Convert a torch LongTensor to a 1D numpy array."""
-        return arr.squeeze().cpu().numpy()
+        try:
+            return arr.squeeze().cpu().numpy()
+        except AttributeError:
+            # When arr is wrapped in a Variable
+            return arr.squeeze().cpu().data.numpy()
 
